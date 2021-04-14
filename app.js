c0ea0465130113dbad7e5ebb5e8f566fd547a096
mohannadateyyat <>
2021-04-14 12:17:19 +0000

    Finsh

--- app.js
+++ app.js
@@ -12,7 +12,4 @@ else if(grade<=70) { document.write('congrats you passed the exam');
 document.write('<img src="https://s10.tvp.pl/images2/0/4/7/uid_047deaa1ca2c945c9ad27a6af34b78781530618716887_width_907_play_0_pos_0_gs_0_height_515.jpg">');}
 
 else if (grade<=100) { document.write('You passed the exam With distinction.'); 
-document.write ('< img src=" https://i.ytimg.com/vi/VR6A4gp9mqA/maxresdefault.jpg">');
-
-
-}
+document.write('<img src=" https://i.ytimg.com/vi/VR6A4gp9mqA/maxresdefault.jpg">');}
