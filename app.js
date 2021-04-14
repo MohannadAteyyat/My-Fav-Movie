5842eb2f6d6e24e8cd4483210f53e203a4c267d5
mohannadateyyat <>
2021-04-14 12:00:19 +0000

    testing

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log('Hi My name im Mohannad and I will be taning today')
+console.log('Think mathmatacly and solve this:')
+var theName=8;
+var theNumber=7;
+console.log(theName+theNumber)
+var grade=prompt('please type your overall grade here from 100');
+if (grade <= 49) {document.write ('Sorry You failed you have to retake the exame') 
+document.write ( '<img src="https://www.fastweb.com/uploads/article_photo/photo/2036449/crop635w_horizontalcropfailSmall.jpg">')
+}
+
+else if(grade<=70) { document.write('congrats you passed the exam');
+document.write('<img src="https://s10.tvp.pl/images2/0/4/7/uid_047deaa1ca2c945c9ad27a6af34b78781530618716887_width_907_play_0_pos_0_gs_0_height_515.jpg">');}
+
+else if (grade<=100) { document.write('You passed the exam With distinction.'); 
+document.write ('< img src=" https://i.ytimg.com/vi/VR6A4gp9mqA/maxresdefault.jpg">');
+
+
+}
