bc0fb85e7b8cb5d9af7c85ab4e67470322ddd331
mohannadateyyat <>
2021-04-15 11:02:58 +0000

    Finishing

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-console.log('Hi My name im Mohannad and I will be taning today')
-console.log('Think mathmatacly and solve this:')
-var theName=8;
-var theNumber=7;
-console.log(theName+theNumber)
-var grade=prompt('please type your overall grade here from 100');
-if (grade <= 49) {document.write ('Sorry You failed you have to retake the exame') 
-document.write ( '<img src="https://www.fastweb.com/uploads/article_photo/photo/2036449/crop635w_horizontalcropfailSmall.jpg">')
+var movieName= prompt('Hello, please choose your fav will smith movie name Focus or Hitch or Hancock');
+
+while(movieName !== 'Focus'&& movieName !== 'Hitch' && movieName!== 'Hancock'){
+  movieName = prompt('Please only enter the name Focus or Hitch or Hancock ');
 }
 
-else if(grade<=70) { document.write('congrats you passed the exam');
-document.write('<img src="https://s10.tvp.pl/images2/0/4/7/uid_047deaa1ca2c945c9ad27a6af34b78781530618716887_width_907_play_0_pos_0_gs_0_height_515.jpg">');}
 
-else if (grade<=100) { document.write('You passed the exam With distinction.'); 
-document.write('<img src=" https://i.ytimg.com/vi/VR6A4gp9mqA/maxresdefault.jpg">');}
+var userChoose;
+
+if(movieName === 'Focus'){
+userChoose='<img src="http://static.rogerebert.com/uploads/movie/movie_poster/focus-2015/large_focus_ver2.jpg">';
+}else if (movieName === 'Hitch'){
+  userChoose='<img src="https://resizing.flixster.com/w_d4Je0ldmFkJdQ2Q-Eg1CdjzhA=/206x305/v2/https://flxt.tmsimg.com/assets/p35562_p_v10_bb.jpg">';
+}
+else if (movieName === 'Hancock'){
+  userChoose='<img src="https://flxt.tmsimg.com/assets/p176343_p_v10_aa.jpg">';
+}
+
+
+var imagesNumber= prompt('How many image do you want?');
+
+for(var i=0 ; i < imagesNumber; i++){
+  document.write(userChoose);
+}
---
+++ lap4.js
@@ -0,0 +1,15 @@
+console.log('Hi My name im Mohannad and I will be taning today')
+console.log('Think mathmatacly and solve this:')
+var theNumber=8;
+var theNumber=7;
+console.log(theNumber+theNumber)
+var grade=prompt('please type your overall grade here from 100');
+if (grade <= 49) {document.write ('<h1>' + 'Sorry You failed you have to retake the exam' + '</h1>' ) 
+document.write ( '<img src="https://www.fastweb.com/uploads/article_photo/photo/2036449/crop635w_horizontalcropfailSmall.jpg">')
+}
+
+else if(grade<=70) { document.write ('<h1>' + 'congrats you passed the exam' + '</h1>');
+document.write('<img src="https://s10.tvp.pl/images2/0/4/7/uid_047deaa1ca2c945c9ad27a6af34b78781530618716887_width_907_play_0_pos_0_gs_0_height_515.jpg">');}
+
+else if (grade<=100) { document.write( '<h1>' + 'You passed the exam With distinction.' + '</h1>'); 
+document.write('<img src=" https://i.ytimg.com/vi/VR6A4gp9mqA/maxresdefault.jpg">');}
